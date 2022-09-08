b618b4fc4f2b2ba61dea8d69d068a96e52408aab
Ashfaq <ashfaqparvez666@gmail.com>
2022-09-08 11:53:25 +0530

    Updated CookieUtils

--- src/main/java/utils/CookieUtils.java
+++ src/main/java/utils/CookieUtils.java
@@ -9,8 +9,7 @@ import java.util.List;
 public class CookieUtils {
 
     public List<Cookie> convertRestAssuredCookiesToSeleniumCookies(Cookies cookies) {
-        List<io.restassured.http.Cookie> restAssuredCookies = new ArrayList<>();
-        restAssuredCookies = cookies.asList();
+        List<io.restassured.http.Cookie> restAssuredCookies = cookies.asList();
         List<Cookie> seleniumCookies = new ArrayList<>();
         for (io.restassured.http.Cookie cookie : restAssuredCookies) {
             seleniumCookies.add(new Cookie(cookie.getName(), cookie.getValue(), cookie.getDomain(), cookie.getPath(),
